a9d4fde82c2dbf6c0933dece5bf6c918dfc49f87
Velmeri <danielmikhh2gmail.com>
2023-04-07 18:47:28 +0200

    Добавьте файлы проекта.

---
+++ 2HW240323/Furniture.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2HW240323
+{
+    class Building
+    {
+        public string Name { get; set; }
+
+        public List<Room> Rooms { get; set; }
+
+        public Building (string name, List<Room> rooms)
+        {
+            Name = name;
+            Rooms = rooms;
+        }
+
+		// Метод для расчета общей стоимости здания
+		public double GetPrice()
+        {
+            double price = 0;
+            foreach(Room room in Rooms) price += room.GetPrice();
+            return price;
+        }
+    }
+
+
+	public class Room
+    {
+        public string Name { get; set; }
+
+        public List<Furniture> Furnitures { get; set; }
+
+        public Room(List<Furniture> furnitures)
+        {
+            Furnitures = furnitures;
+        }
+
+		// Метод для расчета общей стоимости мебели в комнате
+		public double GetPrice()
+        {
+            double price = 0;
+            foreach(Furniture furniture in Furnitures) price += furniture.Price;
+            return price;
+        }
+
+    }
+
+    public class Furniture
+    {
+        public string Name { get; set; }
+        
+        public double Price { get; set; }
+
+        public Furniture(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
---
+++ 2HW240323/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2HW240323
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			// Создание объектов мебели
+			Furniture chair = new Furniture("Chair", 50.00);
+			Furniture table = new Furniture("Table", 100.00);
+			Furniture sofa = new Furniture("Sofa", 200.00);
+
+			// Создание списков мебели для каждой комнаты
+			List<Furniture> livingRoomFurniture = new List<Furniture>() { chair, table, sofa };
+			Room livingRoom = new Room(livingRoomFurniture);
+
+			// Создание объектов комнат с передачей списков мебели в конструкторы
+			List<Furniture> bedroomFurniture = new List<Furniture>() { chair, table };
+			Room bedroom = new Room(bedroomFurniture);
+
+			// Создание списка комнат для здания и добавление объектов комнат
+			List<Room> rooms = new List<Room>() { livingRoom, bedroom };
+
+			// Создание объекта здания с передачей списка комнат и имени в конструктор
+			Building house = new Building("My House", rooms);
+
+			// Вызов метода расчета общей стоимости мебели в здании
+			double totalPrice = house.GetPrice();
+
+			// Вывод общей стоимости мебели в консоль
+			Console.WriteLine($"The total price of all the furniture in the house is: {totalPrice}");
+
+			// Финиш
+			Console.ReadKey(true);
+		}
+    }
+}
